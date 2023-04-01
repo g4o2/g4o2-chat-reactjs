fa542a1e752e87c00617d9fa3e6d30e96748ef89
Hu Kaixiang <maxhu787@gmail.com>
2023-04-01 17:06:36 +0800

    e

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
-      test
+      <Navbar />
+      <div className='container'>
+        test
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import '../styles/navbar.css';
+
+export default function Navbar() {
+    return (
+        <nav>
+            <a href="/" className='logo-name'>
+                G4o2 Chat
+            </a>
+            <div className="nav-menu">
+                <ul>
+                    <li><a>Test</a></li>
+                    <li><a>Test</a></li>
+                    <li><a>Test</a></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
